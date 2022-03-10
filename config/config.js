cdcab6f42443ff92e44375427f7dfdc800376265
yizfar <55544593+yizfar@users.noreply.github.com>
2022-03-10 18:57:16 +0200

    Delete config.js

--- config/config.js
+++
@@ -1,9 +0,0 @@
-exports.secret = {
-    userDb:"yiz10",
-    passDb:"yiz123",
-    jwtSecret:"Monkeyss",
-    EmailFrom:'yizfar@outlook.com',
-    EmailTo:'yizfar@gmail.com',
-    EmailPass:'porush10'
-  }
-  
